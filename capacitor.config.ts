01547c35a537cca96e1ba108817174a573c4e620
arianacatani2009 <mayerlicatani@gmail.com>
2024-05-14 22:35:53 -0500

    splash screem

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,26 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+/// <reference types="@capacitor/splash-screen" />
+
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'photo-galery',
-  webDir: 'www'
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
